94c4b4dbb85546bacbd8d8b4e8b6c5e7edb90a02
InouverS <fabiano.agripino@outlook.com>
2022-08-19 07:57:56 -0300

    123456

---
+++ fib.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int fib(int n){
+  if ( n <= 2 ){
+    return 1;
+  }else {
+    return fib (n-1) + fib (n-2);
+  }
+}
+
+int main() {
+  int n;
+  cout << "Informe o valores de n: " << endl;
+    cin >> n;
+  cout << "Resultado: " << fib(n) << endl;
+  
+}
